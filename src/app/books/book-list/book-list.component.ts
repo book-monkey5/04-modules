7df901e54abf0421230b80d72411ff23faa9b15e
The Buildbot <buildbot@angular2buch.de>
2022-10-28 21:41:38 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/buch/commit/ce9c188ecdef38cd9025354112cfcd743aec4001

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -19,7 +19,7 @@ export class BookListComponent {
         authors: ['Mrs Chimp', 'Mr Gorilla'],
         published: '2022-06-20',
         subtitle: 'Rezepte von Affe bis Zebra',
-        thumbnailUrl: 'https://ng-buch.de/kochen.jpg',
+        thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png',
         description: 'Immer lecker und gut'
       },
       {
@@ -28,7 +28,7 @@ export class BookListComponent {
         authors: ['Orang Utan'],
         published: '2022-07-15',
         subtitle: 'Bananenbrot und mehr',
-        thumbnailUrl: 'https://ng-buch.de/backen.jpg',
+        thumbnailUrl: 'https://cdn.ng-buch.de/backen.png',
         description: 'Tolle Backtipps für Mensch und Tier'
       }
     ];
